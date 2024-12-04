51fc69eecbf3ec7c663b156ef04134ab1949d5ff
Ved Anumala <anuamala.v@northeastern.edu>
2024-12-03 19:38:17 -0500

    Added other group codebase to git

---
+++ src/model/Player.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * Represents a Player in Three Trios by its color.
+ */
+public enum Player {
+  RED,
+  BLUE;
+
+  @Override
+  public String toString() {
+    switch (this) {
+      case RED:
+        return "Red";
+      case BLUE:
+        return "Blue";
+      default:
+        throw new IllegalArgumentException("Invalid player");
+    }
+  }
+}
